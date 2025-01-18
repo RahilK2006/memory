11bafe427aacf8ce4112a9260bd47562e51e1ef6
DESKTOP-73HQ3LE\Utente <mattiadima06@gmail.com>
2025-01-18 11:31:25 +0100

    Revert "Ho creato la matrice per le carte"
    This reverts commit 93898f07ad378be8d895f38a51bd58e01050d77f.

--- Server/MainServer.java
+++ Server/MainServer.java
@@ -3,11 +3,6 @@ import java.net.DatagramSocket;
 
 public class MainServer {
     public static void main(String[] args) throws IOException {
-        Matrice m = new Matrice();
-        m.genera();
-        m.visualizza();
-        
-
         DatagramSocket serverSocket = new DatagramSocket(1234);
 
         Clients listaClienti = Connessione.connessione(serverSocket);
